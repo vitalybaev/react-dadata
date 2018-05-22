14fd0da399be1c89fade4c256ec283cb77de5947
Vitaly Baev <dj@vitalybaev.ru>
2018-05-22 09:12:20 +0300

    Removed unnecessary dependencies

--- webpack.common.js
+++ webpack.common.js
@@ -1,17 +1,12 @@
 const path = require('path');
 const ExtractTextPlugin = require("extract-text-webpack-plugin");
 
-const extractLess = new ExtractTextPlugin({
-  filename: "styles.css",
-  disable: process.env.NODE_ENV === "development"
-});
-
 module.exports = {
   entry: ['./dist/index.js'],
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'index.js',
-    libraryTarget: 'commonjs2' // THIS IS THE MOST IMPORTANT LINE! :mindblow: I wasted more than 2 days until realize this was the line most important in all this guide.
+    libraryTarget: 'commonjs2'
   },
   module: {
     rules: [
@@ -22,28 +17,10 @@ module.exports = {
         use: {
           loader: 'babel-loader'
         }
-      },
-      {
-        test: /\.less$|\.css$/,
-        use: extractLess.extract({
-          use: [{
-            loader: "css-loader",
-            options: {
-              minimize: true
-            }
-          }, {
-            loader: "less-loader"
-          }],
-          // use style-loader in development
-          fallback: "style-loader"
-        })
       }
     ]
   },
   externals: {
     'react': 'commonjs react' // this line is just to use the React dependency of our parent-testing-project instead of using our own React.
-  },
-  plugins: [
-    extractLess
-  ]
+  }
 };
