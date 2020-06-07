a3c83a554400b0ba60620baa62d4e74f5a9006c4
Vitaly Baev <hello@vitalybaev.ru>
2020-06-08 00:13:55 +0300

    Добавлен тип DaDataSuggestion в экспорт

--- src/index.tsx
+++ src/index.tsx
@@ -14,6 +14,7 @@ type DaDataAddressSuggestion = DaDataSuggestion<DaDataAddress>;
 type DaDataPartySuggestion = DaDataSuggestion<DaDataParty>;
 
 export {
+  DaDataSuggestion,
   AddressSuggestions,
   PartySuggestions,
   DaDataAddress,
